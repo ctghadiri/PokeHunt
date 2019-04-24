6faedf631ad482b46fedb76aba4223554d9314fc
ctghadiri <ctghadiri@gmail.com>
2019-04-23 23:19:35 -0700

    create div with className card

--- poke-hunt/src/components/PokeCard/index.js
+++ poke-hunt/src/components/PokeCard/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./style.css"; 
 
 function PokeCard(props){
-
+    return (
+        <div className="card">
+        </div>
+    );
 };
 
 export default PokeCard;
