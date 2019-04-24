ac6b362e196b5711584299f8c11ed022f3040532
ctghadiri <ctghadiri@gmail.com>
2019-04-24 16:16:06 -0700

    export score

--- poke-hunt/src/components/score/index.js
+++ poke-hunt/src/components/score/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
 function Score (props){
     <div className="score">
         <p>Score : {props}</p>
     </div>
-}
+};
+
+export default Score;
