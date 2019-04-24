ad577c297d91674344ac981d68cde116cc899b09
ctghadiri <ctghadiri@gmail.com>
2019-04-24 09:49:00 -0700

    div for image and image tag

--- poke-hunt/src/components/PokeCard/index.js
+++ poke-hunt/src/components/PokeCard/index.js
@@ -4,6 +4,9 @@ import "./style.css";
 function PokeCard(props){
     return (
         <div className="card">
+            <div className="img-container">
+                <img src={props.image} />
+            </div>
         </div>
     );
 };
