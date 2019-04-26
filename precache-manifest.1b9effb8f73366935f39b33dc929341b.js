4699a61cfd659701dabd201e5c89ee04a0edb150
ctghadiri <ctghadiri@gmail.com>
2019-04-26 09:29:52 -0700

    Updates

---
+++ precache-manifest.1b9effb8f73366935f39b33dc929341b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd16e86e749ea7159785445301a9bda4",
+    "url": "/PokeHunt/index.html"
+  },
+  {
+    "revision": "96afd5dec5255c4d0f91",
+    "url": "/PokeHunt/static/css/main.148828e5.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/PokeHunt/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "96afd5dec5255c4d0f91",
+    "url": "/PokeHunt/static/js/main.0ecba45d.chunk.js"
+  },
+  {
+    "revision": "b2e9b4f7d7c14bb343f5",
+    "url": "/PokeHunt/static/js/runtime~main.f1171317.js"
+  }
+]);
