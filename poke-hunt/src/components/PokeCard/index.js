a8670ae74ee8159b50db77b0107229df401e8635
ctghadiri <ctghadiri@gmail.com>
2019-04-25 21:42:16 -0700

    add alt to img tag

--- poke-hunt/src/components/PokeCard/index.js
+++ poke-hunt/src/components/PokeCard/index.js
@@ -5,7 +5,7 @@ function PokeCard(props){
     return (
         <div className="card">
             <div className="img-container">
-                <img src={props.image} />
+                <img src={props.image} alt='Pokemon' />
             </div>
         </div>
     );
