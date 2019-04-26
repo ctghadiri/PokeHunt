ed214cb37d990c5759e4dea656e3b31250c842d0
ctghadiri <ctghadiri@gmail.com>
2019-04-25 21:20:03 -0700

    create shuffle function and adjust catchpokemon function to check for property clicked

--- poke-hunt/src/App.js
+++ poke-hunt/src/App.js
@@ -3,39 +3,45 @@ import PokeCard from "./components/PokeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import pokemon from "./pokemon.json";
+import Score from "./components/Score";
 
 console.log(pokemon)
 
 class App extends Component {
     state = {
         pokemon,
-        caught = [],
-        score = 0
+        score: 0
     }
 
     catchPokemon = id => {
         const pokemonChoice = this.state.pokemon[id-1]
-        if(this.state.caught.includes(pokemonChoice)){
+        if(this.state.pokemon.clicked){
             this.setState({
-                caught: [],
                 score: 0
-            });
+            })
+            this.state.pokemon.forEach(pokemon =>{
+                pokemon.clicked = false
+            })
+            // this.setState(this.shuffle(state.pokemon))
         }
         else {
-            this.setState(state => {
-                const caught = state.caught.concat(pokemonChoice)
-
-                return {
-                    caught,
-                    score: state.score + 1
-                }
-            });
+                this.setState( {
+                    pokemon: this.state.pokemon.clicked = true,
+                    score: this.state.score + 1
+                })
+                this.setState({pokemon: clicked = true})
+            }
+            // this.setState(this.shuffle(state.pokemon))
         }
-        this.setState({caught})
+        // this.setState({caught})
+    // }
+
+    shuffle = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
     }
-
-    function shuffle ()
-
 
     render () {
         return(
