ff0c43342ef534361d618c6bae60bb46e9f017dc
ctghadiri <ctghadiri@gmail.com>
2019-04-25 23:33:39 -0700

    add return to function catchPokemon to set state of json pokemon and move shuffle method above return

--- poke-hunt/src/App.js
+++ poke-hunt/src/App.js
@@ -9,6 +9,7 @@ console.log(pokemon)
 
 class App extends Component {
     state = {
+        // need to set something to allow the pokemon json to be seen as an array rather than a string
         pokemon,
         score: 0
     }
@@ -17,24 +18,22 @@ class App extends Component {
         const pokemonChoice = this.state.pokemon[id-1]
         console.log("hi")
         if(pokemonChoice.clicked){
-            this.setState({
-                score: 0
-            })
+            this.setState({score: 0})
             this.state.pokemon.forEach(pokemon =>{
                 pokemon.clicked = false
             })
-            // this.setState(this.shuffle(state.pokemon))
+            this.setState(this.shuffle(this.state.pokemon))
+            return this.setState({pokemon: pokemon})
         }
         else {
-                this.setState( {
-                    pokemon: pokemonChoice.clicked = true,
-                    score: this.state.score + 1
-                })
+            this.setState( {
+                pokemon: pokemonChoice.clicked = true,
+                score: this.state.score + 1
+            })
+            this.setState(this.shuffle(this.state.pokemon))
+            return this.setState({pokemon: pokemon})
         }
-            // this.setState(this.shuffle(state.pokemon))
     }
-        // this.setState({caught})
-    // }
 
     shuffle = array => {
         for (let i = array.length - 1; i > 0; i--) {
